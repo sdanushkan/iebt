a317fbb42bffa5497bee27a557c12d79cd51f277
danushkan2002 <152047244+sdanushkan@users.noreply.github.com>
2024-07-02 19:47:23 +0530

    v3.6.4

--- frontend/src/screens/DQSecondPage.js
+++ frontend/src/screens/DQSecondPage.js
@@ -38,6 +38,7 @@ import  apply  from '../assets/apply.png'
 import  dq  from '../assets/dq.jpg'
 import  verification  from '../assets/verification.png'
 import  sp  from '../assets/sp.png'
+import  c  from '../assets/c.jpg'
 import  es  from '../assets/es.png'
 import parse from 'html-react-parser';
 
@@ -55,62 +56,83 @@ const DQSecondPage = () => {
   return (
     <div>
         <section className='h-fit w-full relative overflow-hidden'>
-            <div className='h-[400px] lg:h-[500px] w-full object-cover object-bottom absolute z-0 blur-md'>
-            <Swiper
-                slidesPerView={1}
-                spaceBetween={10}
-                pagination={{
-                clickable: true,
-                }}
-                modules={[ Autoplay]}
-                className="w-full"
-                autoplay
-            >
-                <SwiperSlide className='w-full h-full object-contain'>
-                <img src='https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='h-[600px] xl:h-[500px] w-full object-cover object-bottom'/>
-                </SwiperSlide>
-                <SwiperSlide className='w-full h-full object-contain'>
-                <img src='https://media.istockphoto.com/id/1307457391/photo/happy-black-student-raising-arm-to-answer-question-while-attending-class-with-her-university.jpg?s=612x612&w=0&k=20&c=iZaZFyC-WqlqSQc4elqUNPTxLvWPe8P5Tb_YdZnrI9Q=' alt='' className='h-[600px] xl:h-[500px] w-full object-cover object-center'/>
-                </SwiperSlide>
-                {/* <SwiperSlide className='w-full h-full object-contain'>
-                <img src='https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='h-[500px] w-full object-cover object-bottom'/>
-                </SwiperSlide> */}
-            </Swiper>
-            </div>
-
             <div className='h-fit mt-[100px] w-full max-w-[1024px]  relative z-30 px-6 mx-auto object-cover object-bottom flex flex-col md:flex-row items-center py-4'>
-            <div className='h-fit w-full max-w-[1024px] mx-auto px-6 gap-2 flex items-center justify-center'>
-                <div className='h-fit px-8 py-6 backdrop-blur-2xl rounded-[8px] flex flex-col gap-4'>
-                <p className='text-4xl font-bold text-black text-center'>Dual Higher Diploma</p>
-                <p className='text-sm text-black opacity-75 text-center'>Level 4 & 5 Diploma in Business Management</p>
-                
+                <div className='h-fit w-full max-w-[1024px] mx-auto px-6 gap-2 flex items-center justify-center'>
+                    <div className='h-fit flex flex-col gap-4'>
+                    <p className='text-2xl md:text-4xl font-bold text-black text-center'>Dual Higher Diploma</p>                
+                    </div>
                 </div>
-            </div>
             </div>
         </section>
         <section className='h-fit w-full'>
-        <div className='h-fit w-full max-w-[1024px] mx-auto py-[50px] px-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-2'>
-        <div className=' w-full h-full flex flex-col capitalize justify-center py-6 px-4 bg-red-50 rounded-[8px]'>
-            <p className='text-xs font-medium'>Level 5 diploma in</p>
-            <p className='text-sm text-[#DA0C0C] font-bold'>Hospitality and tourism management</p>
-        </div>
-        <div className=' w-full h-full flex flex-col capitalize justify-center py-6 px-4 bg-red-50 rounded-[8px]'>
-            <p className='text-xs font-medium'>Level 5 diploma in</p>
-            <p className='text-sm text-[#DA0C0C] font-bold'>marketing</p>
-        </div>
-        <div className=' w-full h-full flex flex-col capitalize justify-center py-6 px-4 bg-red-50 rounded-[8px]'>
-            <p className='text-xs font-medium'>Level 5 diploma in</p>
-            <p className='text-sm text-[#DA0C0C] font-bold'>human resource management</p>
-        </div>
-        <div className=' w-full h-full flex flex-col capitalize justify-center py-6 px-4 bg-red-50 rounded-[8px]'>
-            <p className='text-xs font-medium'>Level 5 diploma in</p>
-            <p className='text-sm text-[#DA0C0C] font-bold'>logistics and supply chain management</p>
-        </div>
-        <div className=' w-full h-full flex flex-col capitalize justify-center py-6 px-4 bg-red-50 rounded-[8px]'>
-            <p className='text-xs font-medium'>Level 5 diploma in</p>
-            <p className='text-sm text-[#DA0C0C] font-bold'>accounting and finance</p>
-        </div>
-        </div>
+            <div className='h-fit w-full max-w-[1024px] mx-auto px-6 flex flex-row lg:flex-col-reverse lg:items-center'>
+                <div className='h-[250px] lg:h-[50px] w-[10px] lg:w-[5px] bg-[#DA0C0C] mt-auto'></div>
+                <div className='h-fit w-full pl-6 gap-4 flex flex-col'>
+                    <div className='h-[200px] md:h-[250px] w-full  bg-gray-50 overflow-hidden'>
+                        <img src={c} alt='' className='h-full w-full object-cover' />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <p className='text-center text-sm font-medium'>Level 4 and 5 Diploma in Business Management</p>
+                        <p className='text-center text-2xl font-bold mt-4'>Earn your secound Higher Diploma</p>
+                    </div>
+                </div>
+            </div>
+            <div className='h-fit w-full max-w-[1024px] mx-auto px-6 flex lg:flex-col '>
+                <div className='min-h-full w-[10px] lg:min-h-[5px] lg:max-h-[5px] lg:h-[5px] lg:min-w-full lg:w-full bg-[#DA0C0C]'></div>
+                <div className='h-fit w-full gap-4 grid grid-cols-1 lg:grid-cols-5 py-8 lg:py-0 lg:pb-8'>
+
+                    <div className='h-fit w-full gap-2 flex lg:flex-col items-center'>
+                        <div className='h-[5px] lg:h-[25px] w-[25px] lg:w-[5px] bg-[#DA0C0C]'></div>
+                        <div className='h-fit w-full gap-4 flex flex-col'>
+                            <div className='h-[200px] lg:h-[150px] w-full  bg-gray-50 rounded-[8px] overflow-hidden'>
+                                <img src={c} alt='' className='h-full w-full object-cover' />
+                            </div>
+                            <p className='text-left text-sm font-medium'>Level 4 and 5 Diploma in Business Management</p>
+                        </div>
+                    </div>
+
+                    <div className='h-fit w-full gap-2 flex lg:flex-col items-center'>
+                        <div className='h-[5px] lg:h-[25px] w-[25px] lg:w-[5px] bg-[#DA0C0C]'></div>
+                        <div className='h-fit w-full gap-4 flex flex-col'>
+                            <div className='h-[200px] lg:h-[150px] w-full  bg-gray-50 rounded-[8px] overflow-hidden'>
+                                <img src={c} alt='' className='h-full w-full object-cover' />
+                            </div>
+                            <p className='text-left text-sm font-medium'>Level 4 and 5 Diploma in Business Management</p>
+                        </div>
+                    </div>
+
+                    <div className='h-fit w-full gap-2 flex lg:flex-col items-center'>
+                        <div className='h-[5px] lg:h-[25px] w-[25px] lg:w-[5px] bg-[#DA0C0C]'></div>
+                        <div className='h-fit w-full gap-4 flex flex-col'>
+                            <div className='h-[200px] lg:h-[150px] w-full  bg-gray-50 rounded-[8px] overflow-hidden'>
+                                <img src={c} alt='' className='h-full w-full object-cover' />
+                            </div>
+                            <p className='text-left text-sm font-medium'>Level 4 and 5 Diploma in Business Management</p>
+                        </div>
+                    </div>
+
+                    <div className='h-fit w-full gap-2 flex lg:flex-col items-center'>
+                        <div className='h-[5px] lg:h-[25px] w-[25px] lg:w-[5px] bg-[#DA0C0C]'></div>
+                        <div className='h-fit w-full gap-4 flex flex-col'>
+                            <div className='h-[200px] lg:h-[150px] w-full  bg-gray-50 rounded-[8px] overflow-hidden'>
+                                <img src={c} alt='' className='h-full w-full object-cover' />
+                            </div>
+                            <p className='text-left text-sm font-medium'>Level 4 and 5 Diploma in Business Management</p>
+                        </div>
+                    </div>
+
+                    <div className='h-fit w-full gap-2 flex lg:flex-col items-center'>
+                        <div className='h-[5px] lg:h-[25px] w-[25px] lg:w-[5px] bg-[#DA0C0C]'></div>
+                        <div className='h-fit w-full gap-4 flex flex-col'>
+                            <div className='h-[200px] lg:h-[150px] w-full  bg-gray-50 rounded-[8px] overflow-hidden'>
+                                <img src={c} alt='' className='h-full w-full object-cover' />
+                            </div>
+                            <p className='text-left text-sm font-medium'>Level 4 and 5 Diploma in Business Management</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </section>
     </div>
   )
--- frontend/src/screens/DualQualificationScreen.js
+++ frontend/src/screens/DualQualificationScreen.js
@@ -56,39 +56,14 @@ const DualQualificationScreen = () => {
   return (
     <div className='max-w-screen flex flex-col gap-12 pb-12 overflow-x-hidden'>
 
-        <section className='h-fit w-full relative overflow-hidden'>
-
-          <div className='h-[400px] lg:h-[500px] w-full object-cover object-bottom absolute z-0 blur-md'>
-            <Swiper
-              slidesPerView={1}
-              spaceBetween={10}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[ Autoplay]}
-              className="w-full"
-              autoplay
-            >
-              <SwiperSlide className='w-full h-full object-contain'>
-                <img src='https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='h-[600px] xl:h-[500px] w-full object-cover object-bottom'/>
-              </SwiperSlide>
-              <SwiperSlide className='w-full h-full object-contain'>
-                <img src='https://media.istockphoto.com/id/1307457391/photo/happy-black-student-raising-arm-to-answer-question-while-attending-class-with-her-university.jpg?s=612x612&w=0&k=20&c=iZaZFyC-WqlqSQc4elqUNPTxLvWPe8P5Tb_YdZnrI9Q=' alt='' className='h-[600px] xl:h-[500px] w-full object-cover object-center'/>
-              </SwiperSlide>
-              {/* <SwiperSlide className='w-full h-full object-contain'>
-                <img src='https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='h-[500px] w-full object-cover object-bottom'/>
-              </SwiperSlide> */}
-            </Swiper>
-          </div>
-
-          <div className='h-fit mt-[100px] w-full max-w-[1024px]  relative z-30 px-6 mx-auto object-cover object-bottom flex flex-col md:flex-row items-center py-4'>
+        <section className='h-fit w-full relative overflow-hidden mt-[100px]'>
             <div className='h-fit w-full max-w-[1024px] items-center mx-auto px-6 gap-2 grid grid-cols-1 md:grid-cols-2'>
               <div className='h-fit w-full '>
-                  <img src={dq} alt='' className='h-[300px] lg:h-[400px] w-fit object-contain'  />
+                  <img src={dq} alt='' className='h-full md:h-[400px] w-full md:w-fit object-contain'  />
               </div>
-              <div className='h-fit px-8 py-6 backdrop-blur-2xl rounded-[8px] flex flex-col gap-4'>
-                <p className='text-4xl font-bold text-white'>Dual Qualification</p>
-                <p className='text-sm text-white opacity-75'>Pathway to Expertise
+              <div className='h-fit flex flex-col gap-4 '>
+                <p className='text-4xl font-bold text-black'>Dual Qualification</p>
+                <p className='text-sm text-black opacity-75'>Pathway to Expertise
                 The Dual Qualification program offers learners a flexible and comprehensive educational path by combining Level 4 and 5 Higher Diploma studies. Upon completion, students can choose to specialize in various fields, tailoring their education to their career goals. This innovative Pathway to Expertise marks the program as a pioneering initiative in education.</p>
                 <Link to={'/dual/learnmore'}>
                   <Button className='bg-[#DA0C0C] w-fit text-white'>
@@ -97,9 +72,6 @@ const DualQualificationScreen = () => {
                 </Link>
               </div>
             </div>
-          </div>
-
-          
         </section>
         {/* <section className='h-fit w-full'>
 
@@ -119,3 +91,46 @@ const DualQualificationScreen = () => {
 }
 
 export default DualQualificationScreen
+
+
+
+// <div className='h-[400px] lg:h-[500px] w-full object-cover object-bottom absolute z-0 blur-md'>
+//             <Swiper
+//               slidesPerView={1}
+//               spaceBetween={10}
+//               pagination={{
+//                 clickable: true,
+//               }}
+//               modules={[ Autoplay]}
+//               className="w-full"
+//               autoplay
+//             >
+//               <SwiperSlide className='w-full h-full object-contain'>
+//                 <img src='https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='h-[600px] xl:h-[500px] w-full object-cover object-bottom'/>
+//               </SwiperSlide>
+//               <SwiperSlide className='w-full h-full object-contain'>
+//                 <img src='https://media.istockphoto.com/id/1307457391/photo/happy-black-student-raising-arm-to-answer-question-while-attending-class-with-her-university.jpg?s=612x612&w=0&k=20&c=iZaZFyC-WqlqSQc4elqUNPTxLvWPe8P5Tb_YdZnrI9Q=' alt='' className='h-[600px] xl:h-[500px] w-full object-cover object-center'/>
+//               </SwiperSlide>
+//               {/* <SwiperSlide className='w-full h-full object-contain'>
+//                 <img src='https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='h-[500px] w-full object-cover object-bottom'/>
+//               </SwiperSlide> */}
+//             </Swiper>
+//           </div>
+
+//           <div className='h-fit mt-[100px] w-full max-w-[1024px]  relative z-30 px-6 mx-auto object-cover object-bottom flex flex-col md:flex-row items-center py-4'>
+//             <div className='h-fit w-full max-w-[1024px] items-center mx-auto px-6 gap-2 grid grid-cols-1 md:grid-cols-2'>
+//               <div className='h-fit w-full '>
+//                   <img src={dq} alt='' className='h-[300px] lg:h-[400px] w-fit object-contain'  />
+//               </div>
+//               <div className='h-fit px-8 py-6 backdrop-blur-2xl rounded-[8px] flex flex-col gap-4'>
+//                 <p className='text-4xl font-bold text-white'>Dual Qualification</p>
+//                 <p className='text-sm text-white opacity-75'>Pathway to Expertise
+//                 The Dual Qualification program offers learners a flexible and comprehensive educational path by combining Level 4 and 5 Higher Diploma studies. Upon completion, students can choose to specialize in various fields, tailoring their education to their career goals. This innovative Pathway to Expertise marks the program as a pioneering initiative in education.</p>
+//                 <Link to={'/dual/learnmore'}>
+//                   <Button className='bg-[#DA0C0C] w-fit text-white'>
+//                     Learn more
+//                   </Button>
+//                 </Link>
+//               </div>
+//             </div>
+//           </div>
