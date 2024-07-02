273215fbf6e2416f4e48330668a5c041119a91fe
danushkan2002 <152047244+sdanushkan@users.noreply.github.com>
2024-07-02 10:09:26 +0530

    v3.6

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -345,6 +345,7 @@ const Header = () => {
                   
                 </Accordion>
                 <Link onClick={() => setNavbar(!navbar)} to={'/abroad'} className='text-sm font-semibold text-[#DA0C0C] h-12 flex items-center px-2 '>Study abroad</Link>
+                <Link onClick={() => setNavbar(!navbar)} to={'/dual'} className='text-sm font-semibold text-[#DA0C0C] h-12 flex items-center px-2 '>Dual Qualification</Link>
                 <Link onClick={() => setNavbar(!navbar)} to={'/contact'} className='text-sm font-semibold text-[#DA0C0C] h-12 flex items-center px-2 '>Contact us</Link>
               </div>
             </div>
